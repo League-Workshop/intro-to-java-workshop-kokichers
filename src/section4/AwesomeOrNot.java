315c49239fabb2f2acec94faab2efe9a1f4bd402
League Student <league@WTS14-4.attlocal.net>
2019-06-20 14:27:44 -0700

    day 4
    a

--- src/section4/AwesomeOrNot.java
+++ src/section4/AwesomeOrNot.java
@@ -2,32 +2,36 @@ package section4;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class AwesomeOrNot {
 
 	// 1. Make a main method that includes all the steps below…
-
+public static void main(String[] args) {
 	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
-
+int randomNum =new Random().nextInt(4);
 	// 3. Print out this variable
-
+System.out.println(randomNum);
 	// 4. Get the user to enter something that they think is awesome
-
+JOptionPane.showInputDialog("Enter something you believe to be awesome.");
 	// 5. If the random number is 0
-
+if(randomNum==0) {
 	// -- tell the user whatever they entered is awesome!
-
+	JOptionPane.showMessageDialog(null,"Yea,,, that's pretty dang cool.");
 	// 6. If the random number is 1
-
+}else if(randomNum==1){
 	// -- tell the user whatever they entered is ok.
-
+	JOptionPane.showMessageDialog(null,"Ehhh that's okay.");
 	// 7. If the random number is 2
-
+}else if(randomNum==2){
 	// -- tell the user whatever they entered is boring.
-
+	JOptionPane.showMessageDialog(null,"That's boringgg wyd.");
 	// 8. If the random number is 3
-
+}else if(randomNum==3){
 	// -- write your own answer
-
+	JOptionPane.showMessageDialog(null,"Please tell me you're being ironic plea");
+		}
+	}
 }
 
 
--- src/section4/DragonFight.java
+++ src/section4/DragonFight.java
@@ -5,61 +5,75 @@ import javax.swing.JOptionPane;
 import javax.swing.ImageIcon;
 
 public class DragonFight {
-	// 1. Put all of the game code inside the main bethod (below) 
+	// 1. Put all of the game code inside the main method (below) 
 	public static void main(String[] args) {
 		
-		ImageIcon dragon = new ImageIcon("src/section4/dragonPicture.jpg");		
+	ImageIcon dragon = new ImageIcon("src/section4/dragonPicture.jpg");		
 		
-		JOptionPane.showMessageDialog(null, "Defeat the dragon to take its treasure!", "Dragon Fighter", 0, dragon);
+		JOptionPane.showMessageDialog(null, "Defeat the evil yet badly sighted dragon! It has brough much harm upon the nearby village.\nSo you, the unarmed fool who accidentally lead the dragon to everyone in the first place,\nhave been chosen to slay the beast and return with its riches to use for rennovations.", "Dragon Fighter", 0, dragon);
 		// 2. Create a variable called "playerHealth" to store your health (set it equal to 100)
-	
+		int playerHealth = 100;
 		// 3. Create a variable called "dragonHealth" to store the dragon's health (set it equal to 100)
-		
-		// 4. Create a variable to hold the damage the player's attack does each round
-		
-		// 5. Create a variable to hold the damage the dragon's attack does each round
-		
+		int dragonHealth = 100;
+		// 4. Create a variable to hold the damage the player takes each round
+		int playerDamage = 0;
+		// 5. Create a variable to hold the damage the dragon takes does each round
+		int dragonDamage = 0;
 		
 		// 6.  Delete the slashes at the beginning of the next line.  
-		//while(playerHealth>0 && dragonHealth>0) {    //this line of code keeps the battle going until someone's health reaches 0 
-		
+		while(playerHealth>0 && dragonHealth>0) {    //this line of code keeps the battle going until someone's health reaches 0 
 		// 7. Add a closing mustache at the very bottom of this program (since we just added an opening mustache on the previous step).
-		
-		// 8. Ask the player in a pop-up if they want to attack the dragon with a yell or a kick
-		
-		// 9. If they typed in "yell":
-		
-			//-- Find a random number between 0 and 10 and store it in dragonDamage
-			
+
+		// 8. Ask the player in a pop-up if they want to attack the dragon with a dual punches or a swinging kick
+		String attackChoice = JOptionPane.showInputDialog("Will you attack with dual punches or a swinging kick?");
+		// 9. If they typed in "dual punches":
+		if(attackChoice.equals("dual punches")) {
+			//-- Find a random number between 1 and 15 and store it in dragonDamage
+			int randomNumP =new Random().nextInt(15)+1;
+			dragonDamage = randomNumP;
 			//-- Subtract that number from the dragon's health variable 
-			
-		// 10. If they typed in "kick":
-		
-			//-- Find a random number between 0 and 25 and store it in dragonDamage
-			
+			dragonHealth-=dragonDamage;
+			JOptionPane.showMessageDialog(null,"You punch the dragon, dealing "+randomNumP+" damage.");
+			//-- Do this again		
+			int randomNumP2 =new Random().nextInt(15)+1;
+			dragonDamage = randomNumP2;
+			dragonHealth-=dragonDamage;
+			JOptionPane.showMessageDialog(null,"You punch the dragon again, dealing "+randomNumP2+" damage.");
+			// 10. If they typed in "swinging kick":
+		}else if(attackChoice.equals("swinging kick")) {
+			//-- Find a random number between 15 and 25 and store it in dragonDamage
+			int randomNumK =new Random().nextInt(11)+15;
+			dragonDamage = randomNumK;
 			//-- Subtract that number from the dragon's health variable
-			
-			
-		
-		// 11.  Find a random number between 0 and 35 and store it in playerDamage
-		
-		// 12. Subtract this number from the player's health
-		
-		
-		
+			dragonHealth-=dragonDamage;
+			JOptionPane.showMessageDialog(null,"You kick the dragon, dealing a total of "+randomNumK+" damage.");
+		}
+		// Makes a random number generator between just 0 and 1
+		int dragonAim =new Random().nextInt(2);
+		// If the variable is 0, have the dragon miss its attack and tell the player so
+		if(dragonAim == 0) {
+			JOptionPane.showMessageDialog(null,"The dragon swipes at you with its claws, and misses.");
+		}else {
+		// 11.  If the variable is 1, find a random number between 20 and 50 and store it in playerDamage
+			int randomNumC =new Random().nextInt(31)+20;
+			playerDamage = randomNumC;
+			// 12. Subtract this number from the player's health
+			playerHealth-=playerDamage;
+			JOptionPane.showMessageDialog(null,"The dragon swipes at you with its claws, dealing a total of "+randomNumC+" damage.");
 		// 13. If the user's health is less than or equal to 0
-		
+		if(playerHealth<=0) {
 			//-- Tell the user that they lost
-			
-		
+			JOptionPane.showMessageDialog(null,"Unfortunately, you have failed to defeat the dragon. You are dead.");	
 		// 14. Else if the dragon's health is less than or equal to 0
-		
+		}if(dragonHealth<=0) {
 			//-- Tell the user that the dragon is dead and they took a ton of gold!
-			
-	   //  15.  Else
-			
+			JOptionPane.showMessageDialog(null,"Congratulations, you have successfully defeated the dragon and taken much of its gold! Maybe now your family will forgive you for your wrongdoings.");
+	    //  15.  Else
+		}else {
 			//-- Pop up a message that tells the their current health and the dragon's currently health (Bonus: Also display the amount of health that was lost for each player this round)
-			
+		    JOptionPane.showMessageDialog(null,"Current player health is "+playerHealth+" out of 100 while current dragon health is "+dragonHealth+" out of 100.");
+				}
+			}
 		}
 	}
-
+}
